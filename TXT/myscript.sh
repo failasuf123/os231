fcc200f1e7b9b9a889e8c4f9d44fb47aded3abe4
failasuf123 <97381822+failasuf123@users.noreply.github.com>
2023-02-26 14:23:46 +0700

    new REC2

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 # Copyright (C) 2020-2023 Cicak Bin Kadal
 
@@ -21,7 +20,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="60914D29C01C81F1"
+REC2="732CF60922279F6D"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
